2c46074f9ceb50e7da2f0ab782bc2159173c4595
alina <alina.pdlsn@gmail.com>
2022-05-02 15:36:58 +0300

    update

--- src/com/company/Math/Pizzeria.java
+++ src/com/company/Math/Pizzeria.java
@@ -3,14 +3,14 @@ package com.company.Math;
 public class Pizzeria {
     public void makePizza() {
         Pizza[] pizzeria = new Pizza[5];
-        pizzeria[0] = new Pizza("Te la diamo calda", "", 0, 1);
-        pizzeria[1] = new Pizza("Pazzi per la pizza", "", 0, 1);
-        pizzeria[2] = new Pizza("Il pizzavendolo", "", 0, 1);
-        pizzeria[3] = new Pizza("E che pizza!", "", 0, 1);
-        pizzeria[4] = new Pizza("Antica Pizzeria", "", 0, 1);
+        pizzeria[0] = new Pizza("Te la diamo calda", "", 100, 1);
+        pizzeria[1] = new Pizza("Pazzi per la pizza", "", 200, 1);
+        pizzeria[2] = new Pizza("Il pizzavendolo", "", 150, 1);
+        pizzeria[3] = new Pizza("E che pizza!", "", 140, 1);
+        pizzeria[4] = new Pizza("Antica Pizzeria", "", 120, 1);
 
         for (int i = 0; i < 5; i++) {
-            System.out.println(pizzeria[i]);
+            System.out.println(pizzeria[i].name + " " + pizzeria[i].cost);
         }
     }
 }
